1b84a312a29653cff196541d8771dac0721e02e8
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-10-24 23:19:38 +0300

    add changes files

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -8,12 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
+    @Test
+    public void shouldUseConstructor(){
+        Radio radio = new Radio(15);
+        assertEquals(15, radio.getCountRadioBox());
+
+    }
+    @Test
+    public void shouldCountTenRadBox(){
+        Radio radio = new Radio();
+        assertEquals(10, 10);
+    }
+
     @ParameterizedTest
     @CsvSource(value = {"'CurrentRadBox equal nine ', 9, 9",
-            "'CurrentRadBox more than nine', 10, 0",
+            "'CurrentRadBox more than nine', 10, 4",
             "'CurrentRadBox between zero and nine', 4, 4",
             "'CurrentRadBox equal zero', 0, 0",
-            "'CurrentRadBox less than zero', -1, 0"
+            "'CurrentRadBox less than zero', -1, 4"
     })
     void currentRadio(String testName, int setCurrentRadBox, int expected) {
         Radio rad = new Radio();
@@ -30,7 +42,7 @@ class RadioTest {
             "'CurrentRadBox  equal zero', 0, 1"
     })
     void nextRadStat(String testName, int setCurrentRadBox, int expected) {
-        Radio rad = new Radio();
+        Radio rad = new Radio(25);
         rad.setCurrentRadBox(setCurrentRadBox);
         rad.nextRadStat();
 
@@ -55,15 +67,15 @@ class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"'CurrentRadVolume equal ten ', 10, 10",
-            "'CurrentRadVolume more than ten', 11, 10",
-            "'CurrentRadVolume between zero and ten', 4, 4",
+    @CsvSource(value = {"'CurrentRadVolume equal hundred ', 100, 100",
+            "'CurrentRadVolume more than hundred', 101, 100",
+            "'CurrentRadVolume between zero and hundred', 60, 60",
             "'CurrentRadVolume equal zero', 0, 0",
             "'CurrentRadVolume less than zero', -1, 0"
     })
-    void radVolume(String testName, int setCurrentRadBox, int expected) {
+    void radVolume(String testName, int setCurrentRadVolume, int expected) {
         Radio rad = new Radio();
-        rad.setCurrentRadVolume(setCurrentRadBox);
+        rad.setCurrentRadVolume(setCurrentRadVolume);
 
         int actual = rad.getCurrentRadVolume();
 
@@ -71,14 +83,14 @@ class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"'CurrentRadVolume equal ten ', 10, 10",
-            "'CurrentRadVolume more than ten', 11, 10",
-            "'CurrentRadVolume between zero and ten', 7, 8",
+    @CsvSource(value = {"'CurrentRadVolume equal hundred ', 100, 100",
+            "'CurrentRadVolume more than hundred', 101, 100",
+            "'CurrentRadVolume between zero and hundred', 55, 56",
             "'CurrentRadVolume equal zero', 0, 1"
     })
-    void plusVolume(String testName, int setCurrentRadBox, int expected) {
+    void plusVolume(String testName, int currentRadVolume, int expected) {
         Radio rad = new Radio();
-        rad.setCurrentRadVolume(setCurrentRadBox);
+        rad.setCurrentRadVolume(currentRadVolume);
         rad.plusVolume();
 
         int actual = rad.getCurrentRadVolume();
@@ -87,14 +99,14 @@ class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"'CurrentRadVolume equal ten ', 10, 9",
-            "'CurrentRadVolume between zero and ten', 5, 4",
+    @CsvSource(value = {"'CurrentRadVolume equal hundred ', 100, 99",
+            "'CurrentRadVolume between zero and hundred', 50, 49",
             "'CurrentRadVolume equal zero', 0, 0",
             "'CurrentRadVolume less than zero', -1, 0"
     })
-    void minusVolume(String testName, int setCurrentRadBox, int expected) {
+    void minusVolume(String testName, int setCurrentRadVolume, int expected) {
         Radio rad = new Radio();
-        rad.setCurrentRadVolume(setCurrentRadBox);
+        rad.setCurrentRadVolume(setCurrentRadVolume);
         rad.minusRadVolume();
 
         int actual = rad.getCurrentRadVolume();
